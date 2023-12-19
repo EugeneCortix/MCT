a1e6697cc5202f0f9496a2fe7db9edf03af7166b
kyleerod <kyleerod@student.21-school.ru>
2023-12-19 22:42:52 +0700

    backward

--- __main__.py
+++ __main__.py
@@ -1,6 +1,5 @@
-import numpy
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import random
 
 A = []
 x = []
@@ -51,6 +50,14 @@ def predict():
     t3 += b3
     #z = softmax(t2)
     return t3
+def to_full_batch(y, num_classes):
+    y_full = np.zeros((len(y), num_classes))
+    for j, yj in enumerate(y):
+        y_full[j, yj] = 1
+    return y_full
+
+def relu_deriv(t):
+    return (t >= 0).astype(float)
 
 def read_dataset():
     dset = []
@@ -85,5 +92,42 @@ W3 = np.random.randn(OUT_DIM, 1)
 b1 = np.random.randn(H_DIM + 1)
 b2 = np.random.randn(OUT_DIM)
 b3 = np.random.randn(OUT_DIM).reshape(OUT_DIM, 1)
+ALPHA = 0.0002
+NUM_EPOCHS = 400
+BATCH_SIZE = 50
+for ep in range(NUM_EPOCHS):
+    random.shuffle(dataset)
+    for i in range(len(dataset) // BATCH_SIZE):
+
+        batch_x, batch_y = zip(*dataset[i*BATCH_SIZE : i*BATCH_SIZE+BATCH_SIZE])
+        x = np.concatenate(batch_x, axis=0)
+        y = np.array(batch_y)
+
+        # Forward
+        t1 = x @ W1 + b1
+        h1 = relu(t1)
+        t2 = h1 @ W2 + b2
+        h2 = relu(t2)
+        t3 = h2 @ W3
+        t3 += b3
+       # z = softmax_batch(t2)
+       # E = np.sum(sparse_cross_entropy_batch(z, y))
+
+        # Backward
+        y_full = to_full_batch(y, OUT_DIM)
+        dE_dt2 = t3 - y_full
+        dE_dW2 = h1.T @ dE_dt2
+        dE_db2 = np.sum(dE_dt2, axis=0, keepdims=True)
+        dE_dh1 = dE_dt2 @ W2.T
+        dE_dt1 = dE_dh1 * relu_deriv(t1)
+        dE_dW1 = x.T @ dE_dt1
+        dE_db1 = np.sum(dE_dt1, axis=0, keepdims=True)
+
+        # Update
+        W1 = W1 - ALPHA * dE_dW1
+        b1 = b1 - ALPHA * dE_db1
+        W2 = W2 - ALPHA * dE_dW2
+        b2 = b2 - ALPHA * dE_db2
+
 probs = predict()
 print(probs)
